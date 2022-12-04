7f8cc08ef76dd9e5612f37284ddd0cebae9abdfe
ASADUZZAMAN NOOR <asad.info100@gmail.com>
2022-12-05 01:00:03 +0600

    ToString Method

---
+++ ToString/src/A.java
@@ -0,0 +1,21 @@
+
+public class A {
+	
+	String name ;
+	int age ;
+	
+	A(String name ,  int age){
+		this.name = name;
+		this.age = age;	
+	}
+	
+	@Override
+	public String toString() {
+		return name+" "+age;
+	}
+	
+
+	
+	
+
+}
---
+++ ToString/src/Main.java
@@ -0,0 +1,13 @@
+
+public class Main {
+	
+	public static void main(String[] args) {
+		A ob1 = new A("ALAM", 27);
+		A ob2 = new A("ALAP", 17);
+		
+		System.out.println(ob1);
+		System.out.println(ob2);
+	}
+	
+
+}

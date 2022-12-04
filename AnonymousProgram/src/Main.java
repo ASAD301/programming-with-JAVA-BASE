ba644d6740de076d27ae6e8f651f53596ab43235
ASADUZZAMAN NOOR <asad.info100@gmail.com>
2022-12-05 00:41:32 +0600

    Update Main.java

--- AnonymousProgram/src/Main.java
+++ AnonymousProgram/src/Main.java
@@ -4,7 +4,7 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		A object = new A() { // no name class
+		A object = new A(){ // no name class
 			
 			@Override
 			void display(){
